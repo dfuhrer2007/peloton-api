565c605638d6fd7db3d02ea84a4947ee030a1a41
Doug Fuhrer <dfuhrer2007@gmail.com>
2020-01-28 19:20:34 -0500

    pesudo code for the app

---
+++ peloton/program_to_ical.py
@@ -0,0 +1,12 @@
+#Author: Doug Fuhrer
+#Program: Peloton Program to ICAL
+#Purpose: To take the marathon training program from peloton links and setup ICAL 
+
+#Get the target date for race
+
+#Connect to peloton 
+
+#Get the race program "Road to 26.2"
+
+#Generate google calander entries and export them to your calandar
+
